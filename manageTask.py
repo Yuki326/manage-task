65f971bd12cff45f7bc73ac9e31f5adf0688fe36
Yuki326 <s20a1035@gmail.com>
2022-08-15 15:54:59 +0900

    指定タスクの表示機能、時間の編集機能を追加

--- manageTask.py
+++ manageTask.py
@@ -1,16 +1,18 @@
-from cgitb import reset
-from pydoc import describe
-from timeit import timeit
+import sys
 class Time:
   def __init__(self, hour, minute):
       self.hour = hour
       self.minute = minute
       
-  def addTime(self,toAdd):
+  def addTime(self,toAdd,isPlus):
     sumMinute = self.hour*60
     sumMinute += self.minute
-    sumMinute += toAdd.hour*60
-    sumMinute += toAdd.minute
+    if(isPlus):#todo 正負判定を早めにする
+      sumMinute += toAdd.hour*60
+      sumMinute += toAdd.minute
+    else:
+      sumMinute -= toAdd.hour*60
+      sumMinute -= toAdd.minute
     res = Time(0,0)
     res.hour = int(sumMinute/60)
     res.minute = sumMinute%60
@@ -31,12 +33,9 @@ class Task:
 
   def getTime(self):
     res = Time(0,0)
-    #time = {'hour':0,'minute':0}#todo インターフェース
-    print(self.time)
     if('h' in self.time):
       res.hour = int(self.time.split('h')[0])
       if('m' in self.time):
-        print(self.time.split('h')[1].split('m')[0])
         res.minute = int(self.time.split('h')[1].split('m')[0])
     # else:
     #   res.minute = int(self.time.split('m')[0])
@@ -52,30 +51,73 @@ def getText(data):
     text += data[i].toStr()
   return text
 
-f = open('test.txt', 'r+',encoding="utf-8")
-areas = f.read().split()
-data = []
-tmp={}
-for i in range(len(areas)):
-    if(i%4==0):
-        tmp = Task('','','','')
-        tmp.id=areas[i]
-    if(i%4==1):
-        tmp.description=areas[i]
-    if(i%4==2):
-        tmp.time=areas[i]
-    if(i%4==3):
-        tmp.state = areas[i]
-        data.append(tmp)
-f.close()
-t1 = data[0].getTime()
-t2 = data[1].getTime()
-t = t1.addTime(t2)
-print(t.hour,t.minute)
-data[0].time= t.toStr()
+def getData():
+  f = open('test.txt', 'r+',encoding="utf-8")
+  areas = f.read().split()
+  data = []
+  tmp={}
+  for i in range(len(areas)):
+      if(i%4==0):
+          tmp = Task('','','','')
+          tmp.id=areas[i]
+      if(i%4==1):
+          tmp.description=areas[i]
+      if(i%4==2):
+          tmp.time=areas[i]
+      if(i%4==3):
+          tmp.state = areas[i]
+          data.append(tmp)
+  f.close()
+  return data
 
-f = open('test.txt', 'w',encoding="utf-8")
-f.write(getText(data))
-f.close()
-#print(data)
-#print(getTime(data[1]['time']))
+def updateData(data):
+  text = getText(data)
+  if(type(text)!=str):sys.exit()
+  f = open('test.txt', 'w',encoding="utf-8")#ファイルの中身が消えるので注意
+  f.write(text)
+  f.close()
+
+def getTaskById(data,id):
+  for i in range(len(data)):
+    if data[i].id == id:
+      return i
+  return -1
+
+def getTime(str):
+    res = Time(0,0)
+    if('h' in str):
+      res.hour = int(str.split('h')[0])
+      if('m' in str):
+        res.minute = int(str.split('h')[1].split('m')[0])
+    else:
+      res.minute = int(str.split('m')[0])
+    return res
+
+args = sys.argv
+if(len(args)==1):
+  print('コマンドライン引数を入力してください')
+  sys.exit()
+
+data = getData()
+idx = getTaskById(data,args[1])
+if(idx == -1):
+  data.append(Task(args[1],'#','0m','todo'))
+  updateData(data)
+  idx = len(data)-1
+
+if(len(args)==2):
+  print(data[idx].toStr())
+  sys.exit()
+
+origin = data[idx].getTime()
+symbol = args[2][:1]
+if(symbol=='+' or symbol=='-'):
+  isPlus = (symbol == '+')
+  toAdd=args[2][1:]
+  new = origin.addTime(getTime(toAdd),isPlus)
+  data[idx].time= new.toStr()
+  print(data[idx].toStr())
+  updateData(data)
+  sys.exit()
+
+
