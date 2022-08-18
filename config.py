873d2b3a96485c81ba78246baa97a1a1c463518a
Yuki326 <s20a1035@gmail.com>
2022-08-19 05:31:47 +0900

    managetask.pyが置かれているディレクトリを取得

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-MANAGE_TASK_PATH = os.environ['MANAGE_TASK_PATH']
+MANAGE_TASK_PATH = os.path.dirname(os.path.abspath(__file__)) # manage-taskのパス
 RECORD_FOLDER = 'Record'
 TEXT_FILES = ['taskList.txt','log.txt','forExcel.txt']
 PATH = MANAGE_TASK_PATH+'/'+RECORD_FOLDER
